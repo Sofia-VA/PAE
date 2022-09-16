8f414ba390231417f5e46d146022f98c95a2718a
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-16 07:39:07 -0500

    GET de articulos en la API,  formatos array+json

--- Tarea2/index.js
+++ Tarea2/index.js
@@ -1,5 +1,7 @@
-// 0. Variables de entorno
+// 0. Variables de entorno + librerías
 require('dotenv').config();
+const path = require('path');
+const axios = require('axios').default;
 
 // 1. Express
 const express = require('express');
@@ -30,8 +32,23 @@ app.use('/assets', express.static(path.join(__dirname, 'public')));
 // 8. App.get /
 // 8.1 Fetch noticias
 app.get('/noticias', (req, res) => {
-		// 8.2 Render noticias
-    res.render('index',{
-        title: "AAAA"
+    var format = req.query.format;
+    var search = req.query.search;
+    
+    axios.get(`${process.env.API_URL}?q=${search}&apiKey=${process.env.API_KEY}`)
+    .then((response) => {
+        // console.log(response.data);
+        // Retorna { status: 'ok', totalResults: 0, articles: [] }
+        body = response.data; 
+        news = body.articles;
+        if (format == 'json') res.send(JSON.stringify(news));
+        if (format == 'array') res.send(Object.entries(news));
+    }).catch((error) => {
+        console.error('Error', error);
     });
+
+	// 8.2 Render noticias
+    // res.render('index',{
+    //     title: "Found!"
+    // });
 })

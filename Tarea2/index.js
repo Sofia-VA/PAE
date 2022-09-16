ed2ea94892808114418af092ea195e16599f6b59
Sofía VA <sofia-vargas2001@hotmail.com>
2022-09-16 10:41:11 -0500

    Handlebar muestra noticias según búsqueda en form

--- Tarea2/index.js
+++ Tarea2/index.js
@@ -27,14 +27,22 @@ app.listen(port, () => {
 
 // 7. Static + middleware
 // Static Assets 
-app.use('/assets', express.static(path.join(__dirname, 'public')));
+app.use( express.static(path.join(__dirname, '../public/')));
 
 // 8. App.get /
 // 8.1 Fetch noticias
 app.get('/noticias', (req, res) => {
     var format = req.query.format;
     var search = req.query.search;
-    
+    // No params
+    if (!req.query.search) {
+        res.render('index',{
+            layout : 'main',
+            news: null,
+            totalResults: 0,
+        });
+    }
+    //Params
     axios.get(`${process.env.API_URL}?q=${search}&apiKey=${process.env.API_KEY}`)
     .then((response) => {
         // console.log(response.data);
@@ -43,13 +51,15 @@ app.get('/noticias', (req, res) => {
         news = body.articles;
         if (format == 'json') res.send(JSON.stringify(news));
         if (format == 'array') res.send(Object.entries(news));
+
+        // 8.2 Render noticias
+        res.render('index',{
+            layout : 'main',
+            news: news,
+            totalResults: body.totalResults,
+        });
+
     }).catch((error) => {
         console.error('Error', error);
-    });
-
-	// 8.2 Render noticias
-    res.render('index',{
-        layout : 'main',
-        title: "Found!"
     });
 })

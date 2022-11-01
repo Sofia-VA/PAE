88f2474cf4c027d5fa6ee1c0bf1214e6ed3ad76a
Sofía VA <sofia-vargas2001@hotmail.com>
2022-10-31 23:54:14 -0600

    Idea de modelo + controlador

--- memegenerator-api/src/users/controller.js
+++ memegenerator-api/src/users/controller.js
@@ -1,11 +1,16 @@
+usersModel = require('./model')
+
+
 class UserController {
     getAll(req, res) {
-        res.send('Llegaste al endpoint de users');
+
+        // res.send('Llegaste al endpoint de users');
+        res.send(usersModel.getUsers());
         
     }
     getOne(req, res) {
-        res.send('Llegaste al get one endpoint de users', req.params.id);
-        
+        // res.send('Llegaste al get one endpoint de users', req.params.id);
+        res.send(usersModel.getUser(req.params.id));
     }
 }
 
--- memegenerator-api/src/users/model.js
+++ memegenerator-api/src/users/model.js
@@ -1,1 +1,28 @@
-    
+const database = require('../../database');
+//console.log(database.db());
+const usersCollection = database.db().collection('users');
+
+class UsersModel{
+    getUsers() {
+        try {
+            return usersCollection.find();
+        } catch (error) {
+            console.log('Users not found');
+        }
+        
+    }
+
+    getUser(email) {
+        try {
+            usersCollection.findOne({email: email}, (document) => {
+                console.log(document.name);
+                return tojson(document.name);
+            });
+            
+        } catch (error) {
+            console.log('User not found');
+        }
+    }
+}
+
+module.exports = new UsersModel();

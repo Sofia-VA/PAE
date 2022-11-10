325ca44e2497eb9053d839cb1142e53227d917e8
Sofía VA <sofia-vargas2001@hotmail.com>
2022-11-10 14:18:07 -0600

    Cors + express.json

--- memegenerator-api/database/connect.js
+++ memegenerator-api/database/connect.js
@@ -1,6 +1,5 @@
 // Lo que se quiere es que el cliente se conecte a la base de datos
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
 const uri = process.env.DB_URL;
 
 const client = new MongoClient(uri, { 
--- memegenerator-api/index.js
+++ memegenerator-api/index.js
@@ -16,7 +16,8 @@ const apiRoutes = require('./src/api');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use(cors());
+app.use(express.json());
 app.use('/api', apiRoutes);
 
 app.get('', (req, res) => {
